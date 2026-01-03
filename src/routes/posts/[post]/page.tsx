a76e570a94924ff15e9676bfe1d590a71e2ff017
Yannik Peschke <yannik.peschke@gmail.com>
2026-01-03 17:41:25 +0100

    c

--- src/routes/posts/[post]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from '@modern-js/runtime/router';
-
-const PostRoute = () => {
-  const { post } = useParams();
-
-  return <div>Hello from posts {post}</div>;
-};
-
-export default PostRoute;
